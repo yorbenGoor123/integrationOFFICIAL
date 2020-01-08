4ccbcf6cf3b4a301540a8c9c6b315084b9396629
yorbenGoor123 <yorbengoor5@gmail.com>
2020-01-08 02:13:16 +0100

    initial commit

--- src/dao/DAO.php
+++ src/dao/DAO.php
@@ -13,7 +13,7 @@ class DAO {
 
       $dbHost = getenv('PHP_DB_HOST') ?: " ID282046_fifa.db.webhosting.be";
       $dbName = getenv('PHP_DB_DATABASE') ?: "ID282046_fifa";
-      $dbUser = getenv('PHP_DB_USERNAME') ?: "fifa";
+      $dbUser = getenv('PHP_DB_USERNAME') ?: "ID282046_fifa";
       $dbPass = getenv('PHP_DB_PASSWORD') ?: "root12345";
 
 			self::$sharedPDO = new PDO("mysql:host=" . $dbHost . ";dbname=" . $dbName, $dbUser, $dbPass);
