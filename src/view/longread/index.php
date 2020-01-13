061e9ead5d54bc6f73778f9b47d8cba9813f3a63
yorbenGoor123 <yorbengoor5@gmail.com>
2020-01-13 17:26:04 +0100

    hero image zonder responsiviteit, closes #24

--- src/view/longread/index.php
+++ src/view/longread/index.php
@@ -1,6 +1,6 @@
 <main>
         <section class="landing__page">
-          <div id="particles-js">
+          <div id="particles-js"> </div>
             <div class="landing__page__headset">
               <img class="headset__image" src="assets/images/headset/oasis_0000s_0007_virtual-reality.png" alt="">
               <img class="headset__image" src="assets/images/headset/oasis_0000s_0006_Loading-oasis....png" alt="">
@@ -11,6 +11,14 @@
               <img class="headset__image" src="assets/images/headset/oasis_0000s_0001_Layer-6.png" alt="">
               <img class="headset__image" src="assets/images/headset/oasis_0000s_0000_Layer-7.png" alt="">
           </div>
-        </div>
+            <div class="hero__text">
+            <span class="green__title hero__item">Humanity</span>
+            <p class= "hero__paragraph__sub hero__item">destoyed the world</p>
+            <p class="hero__paragraph__small hero__item">so they created their own world <br> called <span class="green__text">the oasis</span></p>
+          </div>
+
+          <div class="center">
+                    <a href="s"><div class="down"></div></a>
+                  </div>
         </section>
     </main>
