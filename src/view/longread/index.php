e0fe2f00b9445908c53aa23bdf0434b19bc55cd6
yorbenGoor123 <yorbengoor5@gmail.com>
2020-01-13 19:30:01 +0100

    responsiviteit header + coole css features, closes #25 #27

--- src/view/longread/index.php
+++ src/view/longread/index.php
@@ -1,14 +1,15 @@
 <main>
         <section class="landing__page">
           <div id="particles-js"> </div>
+          <div class="hero__wrapper">
             <div class="landing__page__headset">
               <img class="headset__image" src="assets/images/headset/oasis_0000s_0007_virtual-reality.png" alt="">
               <img class="headset__image" src="assets/images/headset/oasis_0000s_0006_Loading-oasis....png" alt="">
-              <img class="headset__image" src="assets/images/headset/oasis_0000s_0005_Layer-1.png" alt="">
-              <img class="headset__image" src="assets/images/headset/oasis_0000s_0004_Layer-2.png" alt="">
-              <img class="headset__image" src="assets/images/headset/oasis_0000s_0003_Layer-5.png" alt="">
+              <img class="headset__image glow" src="assets/images/headset/oasis_0000s_0005_Layer-1.png" alt="">
+              <img class="headset__image glow" src="assets/images/headset/oasis_0000s_0004_Layer-2.png" alt="">
+              <img class="headset__image opacity" src="assets/images/headset/oasis_0000s_0003_Layer-5.png" alt="">
               <img class="headset__image" src="assets/images/headset/oasis_0000s_0002_Layer-3.png" alt="">
-              <img class="headset__image" src="assets/images/headset/oasis_0000s_0001_Layer-6.png" alt="">
+              <img class="headset__image glow" src="assets/images/headset/oasis_0000s_0001_Layer-6.png" alt="">
               <img class="headset__image" src="assets/images/headset/oasis_0000s_0000_Layer-7.png" alt="">
           </div>
             <div class="hero__text">
@@ -16,9 +17,20 @@
             <p class= "hero__paragraph__sub hero__item">destoyed the world</p>
             <p class="hero__paragraph__small hero__item">so they created their own world <br> called <span class="green__text">the oasis</span></p>
           </div>
-
+          </div>
           <div class="center">
                     <a href="s"><div class="down"></div></a>
-                  </div>
+          </div>
+
+          <div class="social__media__desktop">
+            <div class="social__items__desktop">
+                <a class="social__item__desktop" href=""><img class="social__item__desktop__image" src="assets/images/facebook.svg" alt="facebook"></a>
+                <a class="social__item__desktop" href=""><img class="social__item__desktop__image" src="assets/images/insta.svg" alt="instagram"></a>
+                <a class="social__item__desktop" href=""><img class="social__item__desktop__image" src="/assets/images/twitter.svg" alt="twitter"></a>
+            </div>
+
+            <a class="" href=""><img class="" src="assets/images/presented.svg" alt="presented by humo"></a>
+        </div>
+
         </section>
     </main>
