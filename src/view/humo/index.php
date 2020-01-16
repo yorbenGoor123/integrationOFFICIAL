4aace263e448b05a4363cf47d9204f4277881110
yorbenGoor123 <yorbengoor5@gmail.com>
2020-01-16 23:33:59 +0100

    progress landing page

--- src/view/humo/index.php
+++ src/view/humo/index.php
@@ -1,4 +1,5 @@
-<main>
+<main class="main__landingpage">
+  <section>
   <article class="section__future">
     <h2 class="section__future__title">Een kijk op onze dystopische toekomst</h2>
     <p class="section__future__paragraph">Lorem Ipsum is slechts een proeftekst uit het
@@ -7,7 +8,57 @@
     de 16e eeuw</p>
 
     <div class="center">
-      <a href="s"><div class="down"></div></a>
+      <a href=""><div class="down"></div></a>
     </div>
   </article>
+
+  <article class="discount">
+  <p class="discount__paragraph__strong">KRIJG 60% KORTING BIJ DEZEAANKOOP</p>
+  <p class="discount__paragraph">Vind de kortingscode op de flap van humo betreffende deze week</p>
+  </article>
+
+  <h2 class="section__title">Boeken</h2>
+
+  <article class="book__article">
+  <img src="assets/humo/book.svg" alt="book article name">
+
+  <div class="previous__next__buttons">
+  <a class="button__previousAndNext previous" href=""><img class="previous__image" src="assets/humo/arrow.svg" alt="arrow_button"></a>
+  <a class="button__previousAndNext next" href=""><img class="next__image" src="assets/humo/arrow.svg" alt="arrow_button"></a>
+  </div>
+
+  <div class="book__info">
+  <p class="book__title"><strong>Title:</strong> ready player one</p>
+  <p class=book__author>-Author</p>
+  <p class="book__description"><strong>Beschrijving:</strong> Lorem Ipsum is slechts een
+  proeftekst uit het drukkerij- en zetterijwezen.
+  Lorem Ipsum is de standaard proeftekst in deze
+  bedrijfstak sinds de 16e eeuw</p>
+  <p class="book__pages"><strong>Bladzijden:</strong> 330</p>
+
+  <div class="rating__wrapper">
+    <img class="book__rating" src="assets/humo/rating_star.svg" alt="rating star">
+    <img class="book__rating" src="assets/humo/rating_star.svg" alt="rating star">
+    <img class="book__rating" src="assets/humo/rating_star.svg" alt="rating star">
+    <img class="book__rating" src="assets/humo/rating_star.svg" alt="rating star">
+  </div>
+
+  <div class="practicall__info">
+  <span class="practicall__info__price">€15</span>
+  <span class="practicall__info__delivery">Levertermijn 1-3 dagen</span>
+  </div>
+
+<form class=landingpage__form action="">
+  <label class="label__quantity" for="aantal">Aantal</label>
+  <input class="input input__number" type="number">
+
+  <input class="input input__BuyNow" type="submit" value="nu kopen">
+  <input class="input input__Ebook" type="submit" value="Koop e-book €1.99">
+</form>
+</div>
+
+  </article>
+
+  </section>
+
 </main>
