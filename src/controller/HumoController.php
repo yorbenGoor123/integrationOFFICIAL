dfb61dfde988b2d10f913f03eeb8702e60421e04
yorbenGoor123 <yorbengoor5@gmail.com>
2020-01-20 01:22:36 +0100

    resolving issues

--- src/controller/HumoController.php
+++ src/controller/HumoController.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once __DIR__ . '/Controller.php';
-require_once __DIR__ . '/../dao/HumoDAO.php';
+require_once __DIR__ . '/../dao/humoDAO.php';
 
 class HumoController extends Controller {
 
